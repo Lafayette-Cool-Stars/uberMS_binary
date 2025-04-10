55ee996cec832a423093bfb9cbfb11154812f7c4
Jared Sofair <sofairj@lafayette.edu>
2025-04-10 17:15:10 -0400

    Extraneous print statements

--- uberMS_binary/binary/priors.py
+++ uberMS_binary/binary/priors.py
@@ -131,26 +131,6 @@ def determineprior(parname, priorinfo, *args):
         
             vradb = numpyro.deterministic("vrad_b",
                                 vradsys - (vrada - vradsys)/(mass_ratio))
-
-            #print("mass_ratio: ", mass_ratio)
-            #print("type of mass_ratio: ", type(mass_ratio))
-            # print("mass_ratio variance: ", mass_ratio.variance)
-            # print("mass_ratio mean: ", mass_ratio.mean)
-            
-            #print("vradsys: ", vradsys)
-            #print("type of vradsys: ", type(vradsys))
-            # print("vradsys variance: ", vradsys.variance)
-            # print("vradsys mean: ", vradsys.mean)
-            
-            #print("vrada: ", vrada)
-            #print("type of vrada: ", type(vrada))
-            # print("vrada variance: ", vrada.variance)
-            # print("vrada mean: ", vrada.mean)
-
-            #print("vradb: ", vradb)
-            #print("type of vradb: ", type(vradb))
-            # print("vradb variance: ", vradb.variance)
-            # print("vradb mean: ", vradb.mean)
             
             return (mass_ratio, vradsys, vrada, vradb)
         else:
@@ -160,23 +140,6 @@ def determineprior(parname, priorinfo, *args):
         
             vradb = numpyro.deterministic("vrad_b",
                                 vradsys - (vrada - vradsys)/(mass_ratio))
-            
-            #print("mass_ratio: ", mass_ratio)
-            #print("mass_ratio variance: ", mass_ratio.variance)
-            #print("mass_ratio mean: ", mass_ratio.mean)
-            
-            #print("vradsys: ", vradsys)
-            #print("vradsys variance: ", vradsys.variance)
-            #print("vradsys mean: ", vradsys.mean)
-            
-            #print("vrada: ", vrada)
-            #print("vrada variance: ", vrada.variance)
-            #print("vrada mean: ", vrada.mean)
-
-            #print("vradb: ", vradb)
-            #print("vradb variance: ", vradb.variance)
-            #print("vradb mean: ", vradb.mean)
-
 
             return (mass_ratio, vradsys, vrada, vradb)
     
